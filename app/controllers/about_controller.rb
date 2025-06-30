300109f9007667ab5e2f346a53b03da299b80ceb
Pavel Averin <pasha2281337alfa@gmail.com>
2025-07-01 00:56:38 +0900

    feat: add flash messages

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,6 @@
 class AboutController < ApplicationController
   def index
+    flash[:notice] = 'Notice flash in about controller'
+    flash[:alert] = 'Alert flash in about controller'
   end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
   def index
+    flash[:notice] = 'Notice flash in main controller'
+    flash[:alert] = 'Alert flash in main controller'
   end
 end

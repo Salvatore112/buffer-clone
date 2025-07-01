2933e9f2bddc0bd35e9927c67cfad5881aff647c
Pavel Averin <pasha2281337alfa@gmail.com>
2025-07-02 04:06:56 +0900

    feat: add errors display for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,9 +18,11 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_signed!(params[:token], purpose: 'password_reset')
-    return unless @user.update(password_params)
-
-    redirect_to sign_in_path, notice: 'Your password has been successfully changed!'
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: 'Your password has been successfully changed!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
